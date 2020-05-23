8780ff725f2e2fba8db4b597ef4210d6942fc053
Daniel <Daniel.teixeirari@gmail.com>
2020-05-22 23:01:46 -0300

    Refresh token

---
+++ src/main/java/com/daniel/teste/controllers/AuthResource.java
@@ -0,0 +1,28 @@
+package com.daniel.teste.controllers;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daniel.teste.security.JWTUtil;
+import com.daniel.teste.security.UserSS;
+import com.daniel.teste.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
